f873e2e02e20dd6c11a159e4aa11f785d342db40
vipin171 <vipin.gupta@fplabs.tech>
2026-01-19 13:34:56 +0530

    chore: apply security fixes terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,15 @@ resource "aws_iam_policy" "app_policy" {
   "Statement": [
     {
       "Effect": "Allow",
-      "Action": "*",                             # Issue 2: wildcard actions
+      "Action": [
+        "s3:GetObject",
+        "s3:PutObject",
+        "s3:ListBucket",
+        "ec2:DescribeInstances",
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
       "Resource": "*"                            # Issue 3: wildcard resources
     }
   ]
